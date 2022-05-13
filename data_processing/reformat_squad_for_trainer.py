204a4733b0b4503bc861f5826a6342bcbb8c8310
mtzcorporations <matjaz.zupanic1@gmail.com>
2022-05-13 11:07:36 +0200

    Popravek verzije, za prazne odgovore

--- data_processing/reformat_squad_for_trainer.py
+++ data_processing/reformat_squad_for_trainer.py
@@ -18,10 +18,15 @@ def reformat_data(data_path, outputh_path):
                         answers["answer_start"].append(answer["answer_start"])
                 if(pog):
                     trainer_data.append({"id": qa["id"], "title": item["title"], "context": paragraph["context"], "question": qa["question"], "answers": answers})
+                elif(qa["is_impossible"]):
+                    trainer_data.append({"id": qa["id"], "title": item["title"], "context": paragraph["context"],
+                                         "question": qa["question"], "answers": answers})
+
     json_new = {"version": "v2.0", "data": trainer_data}
     with open(outputh_path, "w", encoding="utf8") as f:
         f.write(json.dumps(json_new, ensure_ascii=False))
     return trainer_data
 
-reformat_data('../data_processing/output/dev-v2.0_translated_corrected.json',"../data_processing/output/rf_dev-v2.0_translated_corrected.json")
-#reformat_data( '../data/train-v2.0_translated_correctedSLO.json',"../data_processing/output/rf_train-v2.0_translated_correctedSLO.json")
+
+reformat_data('../data_processing/output/dev-v2.0_translated_corrected.json',"../data_processing/output/rf_dev-v2.0_SLO_translated_corrected.json")
+reformat_data( '../data_processing/output//train-v2.0_translated_corrected.json',"../data_processing/output/rf_train-v2.0_SLO_translated_corrected.json")
